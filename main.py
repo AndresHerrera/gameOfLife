ca432c41f2397a801098fa54b820fa47aa6e249a
AndresHerrera <t763rm3n@gmail.com>
2020-09-24 11:47:31 -0500

    game of life with pygame

---
+++ main.py
@@ -0,0 +1,125 @@
+import sys
+import pygame
+import numpy as np
+import time
+
+def start_game():
+    pygame.init()
+
+    # Sreen Size
+    width, height = 500, 500
+    screen = pygame.display.set_mode((height,width))
+    pygame.display.set_caption("Game of Life")
+
+    # Screen Color
+    bg = 25, 25, 25
+    screen.fill(bg)
+
+    # Num Pixels 
+    nxC, nyC = 50, 50
+    dimCW = width/nxC
+    dimCH = height/nyC
+
+    # Cells game state.  Life=1 ; Death=0
+    gameState = np.zeros((nxC,nyC))
+
+    # initializate automata (palo)
+    gameState[5,3] = 1
+    gameState[5,4] = 1
+    gameState[5,5] = 1
+
+    # initializate automata (movil)
+    gameState[21,21] = 1
+    gameState[22,22] = 1
+    gameState[22,23] = 1
+    gameState[21,23] = 1
+    gameState[20,23] = 1
+
+
+    # initializate automata (R-pentomino)
+    gameState[31,30] = 1
+    gameState[32,30] = 1
+    gameState[30,31] = 1
+    gameState[31,31] = 1
+    gameState[31,32] = 1
+
+    # Excecution control
+    pauseExec = False
+
+
+    # Excecution loop
+    while True:
+
+        newGameState = np.copy(gameState)
+
+        screen.fill(bg)
+        time.sleep(0.1)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                break
+            if event.type == pygame.KEYDOWN:
+                pauseExec = not pauseExec
+            
+            mouseClick = pygame.mouse.get_pressed()
+            if sum(mouseClick) > 0 :
+                posX, posY = pygame.mouse.get_pos()
+                celX, celY = int(np.floor(posX/dimCW)), int(np.floor(posY/dimCH))
+                #change clicked cell
+                newGameState[celX,celY] = not mouseClick[2]
+                
+
+        for y in range(0,nxC):
+            for x in range(0,nyC):
+                
+                if not pauseExec:
+                    # calculate number of closest neighbors 
+                    #n_neigh =   gameState[(x-1),(y-1)] + \
+                    #            gameState[(x),(y-1)] + \
+                    #            gameState[(x+1),(y-1)] + \
+                    #            gameState[(x-1),(y)] + \
+                    #            gameState[(x+1),(y)] + \
+                    #            gameState[(x-1),(y-1)] + \
+                    #            gameState[(x),(y+1)] + \
+                    #            gameState[(x+1),(y+1)] 
+                    
+                    # toroid strategy 
+                    n_neigh =   gameState[(x-1) % nxC ,(y-1) % nyC] + \
+                                gameState[(x)   % nxC ,(y-1) % nyC] + \
+                                gameState[(x+1) % nxC ,(y-1) % nyC] + \
+                                gameState[(x-1) % nxC ,(y)   % nyC] + \
+                                gameState[(x+1) % nxC ,(y)   % nyC] + \
+                                gameState[(x-1) % nxC ,(y+1) % nyC] + \
+                                gameState[(x)   % nxC ,(y+1) % nyC] + \
+                                gameState[(x+1) % nxC ,(y+1) % nyC] 
+                    
+                    # Game Rules
+                    # Rule 1:  Cell death with 3 neighborhoods life, "re-born"
+                    if gameState[x,y] == 0 and n_neigh == 3:
+                        newGameState[x,y] = 1
+                    # Rule 2: Cell life with less than 2 or more than 3 neighborhoods life, "die"
+                    elif gameState[x,y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                        newGameState[x,y] = 0
+
+                # create a polygon for each cell to draw
+                poly=[((x) * dimCW , y * dimCH),
+                        ((x+1) * dimCW , y * dimCH),
+                        ((x+1) * dimCW , (y+1) * dimCH),
+                        ((x) * dimCW , (y+1) * dimCH)
+                    ]
+
+                if newGameState[x,y] == 0:
+                    pygame.draw.polygon(screen,(128,128,128),poly,1)
+                else:
+                    pygame.draw.polygon(screen,(255,255,255),poly,0)    
+            
+        # Update Game state
+        gameState = np.copy(newGameState)
+
+        # Update Screen
+        pygame.display.flip()
+
+        pass
+
+start_game()
